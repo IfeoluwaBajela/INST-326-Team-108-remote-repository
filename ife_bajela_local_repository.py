e2156cb283fde1cd0583f2f057d83aa980ef3076
Ifeoluwa Bajela <ibajela@umd.edu>
2026-03-30 22:13:47 -0400

    *added

---
+++ ife_bajela_local_repository.py
@@ -0,0 +1,8 @@
+""" Pratice adding and commiting to a local repository"""
+class Repository:
+    """A local repository to store my files and edit history
+    
+    Attributes:
+        Name (str): Name of repository 
+        Files(list): List of files in working directorygit
+    """

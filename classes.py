8af72e27bc432f5a9cc0e6d05c4ec01b0c043536
Jeremy Negron-Davila <jnegrond@terpmail.umd.edu>
2026-04-26 02:36:12 -0400

    Added classes files with two files containing innit methods.

---
+++ classes.py
@@ -0,0 +1,15 @@
+class player():
+    """Class for a player. Each player has some sort of name and a highscore 
+    that is updated. 
+    
+    """
+    def __init__(self, name, highscore):
+        pass
+
+
+
+
+class game_state():
+    
+    def __init__(self, word):
+        pass
